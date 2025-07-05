6048665b40be5af16410b153186c9347f181403e
GeanJS <gean.jair@gmail.com>
2025-07-04 22:23:37 -0300

    task class and gitignore

---
+++ model/task.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+class Task:
+    def __init__(self, description: str, status: str, creation_date: datetime, completion_date = None):
+        self.__description = description
+        self.__status = status
+        self.__created = creation_date
+        self.__completed = completion_date
+        
+        
+    @property
+    def description(self) -> str:
+        return self.__description
+    
+    @description.setter
+    def description(self, new_description: str):
+        self.__description = new_description
+        
+    @property
+    def status(self) -> str:
+        return self.__status
+    
+    @status.setter
+    def status(self, new_status: str):
+        self.__status = new_status
+        
+    @property
+    def created(self) -> datetime:
+        return self.__created
+    
+    @property
+    def completed(self):
+        return self.__completed
+    
+    @completed.setter
+    def completed(self, new_date: datetime):
+        self.__completed = new_date
